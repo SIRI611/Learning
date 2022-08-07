4cc30dfbb5a915c32aae09f70269ef03bd549f3b
Siri <sxr-714371558@outlook.com>
2022-08-07 13:59:41 +0800

    insert

---
+++ LSTM初步.py
@@ -0,0 +1,224 @@
+import numpy as np
+import pandas as pd
+
+#输入数据
+filepath = 'D:\杂记\CVIX.xlsx'#大家跑的时候记得把路径改一下
+data = pd.read_excel(filepath)
+data = data.sort_values('日期')
+
+#显示源数据（会弹出来一张表，纵坐标CVIX指数，横坐标为时间，但是数据量太大，我就把横坐标简化了，之后再改为时间）不过这一步也可以不用
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+#sklearn特征工程
+CVIX = data[['上证50ETF隐含波动率指数']]
+CVIX.info()
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(-1, 1))
+CVIX['上证50ETF隐含波动率指数'] = scaler.fit_transform(CVIX['上证50ETF隐含波动率指数'].values.reshape(-1,1))
+
+#构造数据集
+def split_data(stock, lookback):
+    data_raw = stock.to_numpy()
+    data = []
+
+    # you can free play（seq_length）
+    for index in range(len(data_raw) - lookback):
+        data.append(data_raw[index: index + lookback])
+
+    data = np.array(data);
+    test_set_size = int(np.round(0.2 * data.shape[0]))
+    train_set_size = data.shape[0] - (test_set_size)
+
+    x_train = data[:train_set_size, :-1, :]
+    y_train = data[:train_set_size, -1, :]
+
+    x_test = data[train_set_size:, :-1]
+    y_test = data[train_set_size:, -1, :]
+
+    return [x_train, y_train, x_test, y_test]
+
+
+lookback = 20
+x_train, y_train, x_test, y_test = split_data(CVIX, lookback)
+
+#下面构建LSTM模型
+import torch
+import torch.nn as nn
+
+x_train = torch.from_numpy(x_train).type(torch.Tensor)
+x_test = torch.from_numpy(x_test).type(torch.Tensor)
+y_train_lstm = torch.from_numpy(y_train).type(torch.Tensor)
+y_test_lstm = torch.from_numpy(y_test).type(torch.Tensor)
+y_train_gru = torch.from_numpy(y_train).type(torch.Tensor)
+y_test_gru = torch.from_numpy(y_test).type(torch.Tensor)
+
+input_dim = 1
+hidden_dim = 32
+num_layers = 2
+output_dim = 1
+num_epochs = 100
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:, -1, :])
+        return out
+
+model = LSTM(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers)
+criterion = torch.nn.MSELoss()
+optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+
+#对模型进行训练
+import time
+
+hist = np.zeros(num_epochs)
+start_time = time.time()
+lstm = []
+
+for t in range(num_epochs):
+    y_train_pred = model(x_train)
+
+    loss = criterion(y_train_pred, y_train_lstm)
+    print("Epoch ", t, "MSE: ", loss.item())#输出每次训练的MSE
+    hist[t] = loss.item()
+
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
+
+training_time = time.time() - start_time
+print("Training time: {}".format(training_time))
+
+#让结果呈现在一张图表中
+predict = pd.DataFrame(scaler.inverse_transform(y_train_pred.detach().numpy()))
+original = pd.DataFrame(scaler.inverse_transform(y_train_lstm.detach().numpy()))
+import seaborn as sns
+sns.set_style("darkgrid")
+
+fig = plt.figure()
+fig.subplots_adjust(hspace=0.2, wspace=0.2)
+
+plt.subplot(1, 2, 1)
+ax = sns.lineplot(x = original.index, y = original[0], label="Data", color='royalblue')
+ax = sns.lineplot(x = predict.index, y = predict[0], label="Training Prediction (LSTM)", color='tomato')
+ax.set_title('CVIX', size = 14, fontweight='bold')
+ax.set_xlabel("Dates", size = 14)
+ax.set_ylabel("CVIX", size = 14)
+ax.set_xticklabels('', size=10)
+
+
+plt.subplot(1, 2, 2)
+ax = sns.lineplot(data=hist, color='royalblue')
+ax.set_xlabel("Epoch", size = 14)
+ax.set_ylabel("Loss", size = 14)
+ax.set_title("Training Loss", size = 14, fontweight='bold')
+fig.set_figheight(6)
+fig.set_figwidth(16)
+
+#模型检验
+import math, time
+from sklearn.metrics import mean_squared_error
+
+y_test_pred = model(x_test)
+
+y_train_pred = scaler.inverse_transform(y_train_pred.detach().numpy())
+y_train = scaler.inverse_transform(y_train_lstm.detach().numpy())
+y_test_pred = scaler.inverse_transform(y_test_pred.detach().numpy())
+y_test = scaler.inverse_transform(y_test_lstm.detach().numpy())
+
+#计算RMSE均方根误差并输出
+trainScore = math.sqrt(mean_squared_error(y_train[:,0], y_train_pred[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(y_test[:,0], y_test_pred[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+lstm.append(trainScore)
+lstm.append(testScore)
+lstm.append(training_time)
+
+trainPredictPlot = np.empty_like(CVIX)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[lookback:len(y_train_pred)+lookback, :] = y_train_pred
+
+testPredictPlot = np.empty_like(CVIX)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(y_train_pred)+lookback-1:len(CVIX)-1, :] = y_test_pred
+
+original = scaler.inverse_transform(CVIX['上证50ETF隐含波动率指数'].values.reshape(-1,1))
+
+predictions = np.append(trainPredictPlot, testPredictPlot, axis=1)
+predictions = np.append(predictions, original, axis=1)
+result = pd.DataFrame(predictions)
+
+#显示结果
+import plotly.express as px
+import plotly.graph_objects as go
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(go.Scatter(x=result.index, y=result[0],
+                    mode='lines',
+                    name='Train prediction')))
+fig.add_trace(go.Scatter(x=result.index, y=result[1],
+                    mode='lines',
+                    name='Test prediction'))
+fig.add_trace(go.Scatter(go.Scatter(x=result.index, y=result[2],
+                    mode='lines',
+                    name='Actual Value')))
+fig.update_layout(
+    xaxis=dict(
+        showline=True,
+        showgrid=True,
+        showticklabels=False,
+        linecolor='white',
+        linewidth=2
+    ),
+    yaxis=dict(
+        title_text='Close (USD)',
+        titlefont=dict(
+            family='Rockwell',
+            size=12,
+            color='white',
+        ),
+        showline=True,
+        showgrid=True,
+        showticklabels=True,
+        linecolor='white',
+        linewidth=2,
+        ticks='outside',
+        tickfont=dict(
+            family='Rockwell',
+            size=12,
+            color='white',
+        ),
+    ),
+    showlegend=True,
+    template = 'plotly_dark'
+
+)
+
+
+
+annotations = []
+annotations.append(dict(xref='paper', yref='paper', x=0.0, y=1.05,
+                              xanchor='left', yanchor='bottom',
+                              text='Results (LSTM)',
+                              font=dict(family='Rockwell',
+                                        size=26,
+                                        color='white'),
+                              showarrow=False))
+fig.update_layout(annotations=annotations)
+
+fig.show()
